96ea9d8e725d00dfcf828516db2c6c583b42f55d
Rijnard van Tonder <rvantonder@gmail.com>
2019-04-11 15:22:32 +0000

    parse functions that return user state

--- src/mParser.ml
+++ src/mParser.ml
@@ -3,6 +3,7 @@
    -----------------------------------------------------------------------------
    Copyright (C) 2008, Holger Arnold
                  2014-2020, Max Mouratov
+                 2021, Rijnard van Tonder (modifications)
 
    License:
      This library is free software; you can redistribute it and/or
@@ -346,6 +347,25 @@ let parse_string p str user =
 let parse_channel p chn user =
   let input = MParser_Char_Stream.from_channel chn in
   parse p input user
+
+let parse' p input user =
+  match p (init input user) with
+    | Empty_ok (x, s, _) | Consumed_ok (x, s, _) ->
+        Success (x, s.user)
+    | Empty_failed e | Consumed_failed e ->
+        (match e with
+          | Parse_error (pos, messages) ->
+              Failed (error_message input pos messages 78 0, e)
+          | No_error ->
+              Failed ("", e))
+
+let parse_string' p str user =
+  let input = MParser_Char_Stream.from_string str in
+  parse' p input user
+
+let parse_channel' p chn user =
+  let input = MParser_Char_Stream.from_channel chn in
+  parse' p input user
 
 
 (* Parser combinators
